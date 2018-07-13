6764445e2470511d8378e919c9604defcc8d7510
Brent Jackson <jxnblk@gmail.com>
2018-07-13 15:30:07 -0400

    Edit cli

--- cli.js
+++ cli.js
@@ -10,7 +10,7 @@ const initit = require('initit')
 const templates = [
   {
     name: 'Next.js',
-    template: 'jxnblk/create-docs/templates/next'
+    path: 'jxnblk/create-docs/templates/next'
   },
 ]
 
@@ -23,8 +23,27 @@ const cli = meow(`
 
   Options
 
+    --name  Directory name for docs
+
+    -y      Create docs without confirmation step
+
 `, {
   flags: {
+    help: {
+      type: 'boolean',
+      alias: 'h'
+    },
+    version: {
+      type: 'boolean',
+      alias: 'v'
+    },
+    name: {
+      type: 'string'
+    },
+    confirm: {
+      type: 'boolean',
+      alias: 'y'
+    }
   }
 })
 
@@ -49,21 +68,42 @@ const form = [
 ]
 
 const run = async opts => {
+  prompts.inject(opts)
   const response = await prompts(form)
 
   if (!response.confirm) {
     console.log('aborted')
     process.exit(0)
   }
+  const { name } = response
+  const template = templates[response.template]
 
-  if (!response.name) {
-    console.error('name is required')
+  const spinner = ora('creating docs...').start()
+
+  if (!name) {
+    spinner.fail('name is required')
     // todo: prompt again
     process.exit(1)
   }
+
+  if (!template) {
+    // this should never happen
+    spinner.fail('template not found')
+    process.exit(1)
+  }
+
   // todo: ensure directory doesn't exist
 
-  console.log('creating docs', response)
+  initit({ name, template: template.path })
+    .then(res => {
+      spinner.succeed('created docs')
+      process.exit(0)
+    })
+    .catch(err => {
+      spinner.fail('failed to create docs')
+      console.log(chalk.red('ERR'), err)
+      process.exit(1)
+    })
 }
 
-run({})
+run(cli.flags)
